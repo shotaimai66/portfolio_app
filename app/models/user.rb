f58c49286b466f35093df9fa0cc26184da25d9e4
shotaimai <changemymind6@gmail.com>
2018-05-17 10:58:21 +0000

    portofolio

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,6 +100,14 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
   
+   def self.search(search) #ここでのself.はUser.を意味する
+    if search
+      where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。#User.は省略
+    else
+      all #全て表示。#User.は省略
+    end
+  end
+  
   private
 
     # メールアドレスをすべて小文字にする
