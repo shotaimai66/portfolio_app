9215397e5fe91bd7cbeee1d166917f977d17f3c5
shotaimai <changemymind6@gmail.com>
2018-05-17 12:48:21 +0000

    portfolio4

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -5,7 +5,7 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "#{user.name}ようこそProgra:へ！"
+      flash[:success] = "#{user.name}さん！ ようこそProgra:へ！"
       redirect_to user
     else
       flash[:danger] = "アカウント有効化リンクが無効です。"
