12b77f388cf042119ee61b5d698548bf8c198cc4
EC2 Default User <ec2-user@ip-172-31-14-114.us-east-2.compute.internal>
2018-07-07 06:21:02 +0000

    test

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 
 
-module SampleApp
+module Portfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
